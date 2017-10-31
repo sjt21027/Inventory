8d399fc81969ceb8c56add74196c9e1c8f782cd3
this.p1.cpu() <steventhompson@thisp1cpu.local>
2017-10-30 20:13:32 -0400

    Initial Commit

---
+++ P2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  P2
+//
+//  Created by this.p1.cpu(); on 10/30/17.
+//  Copyright © 2017 this.p1.cpu();. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
